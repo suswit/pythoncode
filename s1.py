86b313281bf5f57996394ae48556fa5e678529fc
suswit <suswitl2300@gmail.com>
2023-04-19 16:47:38 +0530

    s12

--- s1.py
+++ s1.py
@@ -5,17 +5,53 @@
 # '''
 # m.mainloop()
 
-import tkinter as tk
+# import tkinter as tk
 
-main=tk.Tk(className="emi calculator")
-main.geometry("600x600")
+# main=tk.Tk(className="emi calculator")
+# main.geometry("600x600")
 
-label1=tk.Label(main,text="python")
-txt1=tk.Entry(main)
-btn1=tk.Button(main,text="submit")
+# label1=tk.Label(main,text="python")
+# txt1=tk.Entry(main)
+# btn1=tk.Button(main,text="submit")
 
-label1.grid(row=0,column=0)
-txt1.grid(row=0,column=1)
-btn1.grid(row=1,column=1)
+# label1.grid(row=0,column=0)
+# txt1.grid(row=0,column=1)
+# btn1.grid(row=1,column=1)
 
-main.mainloop()
+# main.mainloop()
+
+# def my_function():
+#     '''Demonstrate triple double quotes
+#     docstrings and does nothing really.'''
+
+#     return None
+
+# print("Using_Doc_:")
+# print(my_function.__doc__)
+
+# print("Using help:")
+# help(my_function)
+
+# def cube(y):
+#     return y*y*y
+
+# lambda_cube = lambda y: y*y*y
+
+# # using function defined
+# # using def keyword
+# print ("Using function defined with 'def' keyword,cube:",cube(5))
+
+# # using the lambda function
+# print("Using lambda function, cube:",lambda_cube(5))
+
+def add(y):
+    return y+y+y
+
+lambda_cube = lambda y: y+y+y
+
+# using function defined
+# using def keyword
+print ("Using function defined with 'def' keyword,cube:",add(5))
+
+# using the lambda function
+print("Using lambda function, cube:",lambda_cube(5))
